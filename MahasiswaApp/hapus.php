765e33ec79497e8c786b5e80a1d6ab3d7b0d14d7
REHUEL MIRROR <rehuelmirrorpertiwibrsaragi@gmail.com>
2026-01-09 23:54:09 +0700

    Delete MahasiswaApp directory

--- MahasiswaApp/hapus.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'koneksi.php';
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $conn->query("DELETE FROM mahasiswa WHERE ID_Mahasiswa='$id'");
-}
-header("Location: dashboard.php");
-exit;
-?>
